11b2f6158e6b91cb4cdb7d14c703c71a95112dcd
kapis <kapis90@gmail.com>
2016-11-06 12:09:11 +0100

    Add browse, reorganize GUI

--- XML_Manager/XML_Manager.py
+++ XML_Manager/XML_Manager.py
@@ -8,13 +8,16 @@ class XMLManager(QtGui.QMainWindow):
         self.ui = Ui_XMLManager()
         self.ui.setupUi(self)
         self.ui.OkButton.clicked.connect(self.CopyEntry)
+        self.ui.BrowseButton.clicked.connect(self.Browse)
 
     def CopyEntry(self):
         Nametext = self.ui.NameEdit.text()
         self.ui.IterationEdit.setText(Nametext)
 
+    def Browse(self):
+        xmlFile = QtGui.QFileDialog.getOpenFileName(self,'Open File', '', 'XML File(*.xml)')
+        self.ui.XMLPathEdit.setText(xmlFile)
 
-        
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
     myapp = XMLManager()
