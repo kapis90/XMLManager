0309bc07c3b3d1b7929a4dd896f484f3f2917f77
kapis <kapis90@gmail.com>
2016-11-06 10:26:49 +0100

    add working XMLManager Form

--- XML_Manager/XML_Manager.py
+++ XML_Manager/XML_Manager.py
@@ -1,1 +1,15 @@
-TEST
+import sys 
+from PyQt4 import QtCore, QtGui
+from XML_ManagerWindow import Ui_XMLManager
+
+class XMLManager(QtGui.QMainWindow):
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        self.ui = Ui_XMLManager()
+        self.ui.setupUi(self)
+
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    myapp = XMLManager()
+    myapp.show()
+    sys.exit(app.exec_())
