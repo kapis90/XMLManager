537ba7c825ea576c31ca63c9f8c047a70062dae8
kapis <kapis90@gmail.com>
2016-11-14 20:32:38 +0100

    Add list of benchmarks as QTableWidget

--- XML_Manager/XML_Manager.py
+++ XML_Manager/XML_Manager.py
@@ -9,13 +9,9 @@ class XMLManager(QtGui.QMainWindow):
         QtGui.QWidget.__init__(self, parent)
         self.ui = Ui_XMLManager()
         self.ui.setupUi(self)
-        self.ui.OkButton.clicked.connect(lambda: self.addBenchmark({str(self.ui.NameEdit.text()):str(self.ui.IterationEdit.text())}))
-        self.ui.SaveButton.clicked.connect(self.saveFile)
+        #self.ui.OkButton.clicked.connect(lambda: self.addBenchmark({str(self.ui.NameEdit.text()):str(self.ui.IterationEdit.text())}))
+        self.ui.ApplyButton.clicked.connect(self.saveFile)
         self.ui.BrowseButton.clicked.connect(self.browse)
-
-    def copyEntry(self):
-        Nametext = self.ui.NameEdit.text()
-        self.ui.IterationEdit.setText(Nametext)
 
     def browse(self):
         self.xmlFilePath = QtGui.QFileDialog.getOpenFileName(self,'Open File', '', 'XML File(*.xml)')
@@ -24,7 +20,7 @@ class XMLManager(QtGui.QMainWindow):
          
     def parseFile(self):
         self.benchmarkXmlDoc = XML_Parser(str(self.xmlFilePath))
-        print self.benchmarkXmlDoc.benchmarksMap
+        self.__fillTable()
 
     def addBenchmark(self, benchMap):
         self.benchmarkXmlDoc.addBenchmark(benchMap)
@@ -32,8 +28,19 @@ class XMLManager(QtGui.QMainWindow):
     def saveFile(self):
         self.benchmarkXmlDoc.writeFile()
         
-    
+    def __fillTable(self):
 
+        items = self.benchmarkXmlDoc.benchmarksMap.items()
+        self.ui.tableWidget.setRowCount(len(items))
+        self.ui.tableWidget.setColumnCount(2)
+        
+        for i in range(len(items)):
+            self.ui.tableWidget.setItem(i, 0, QtGui.QTableWidgetItem(items[i][0]))
+            self.ui.tableWidget.setItem(i, 1, QtGui.QTableWidgetItem(items[i][1]))
+            
+
+
+        
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
     myapp = XMLManager()
